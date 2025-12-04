ab9bde9cca7f7dceb1394f392e7067727179bb5c
sushmita <testing_engr@m-techindia.com>
2025-12-04 17:27:30 +0530

    code has pushed

--- src/test/java/Report/VehicleStaus.java
+++ src/test/java/Report/VehicleStaus.java
@@ -1,64 +1,65 @@
 package report;
 
 import java.io.IOException;
-import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import baseData.Login;
 import baseData.baseLoginTest;
 
-public class LoginAuditHistory extends baseLoginTest {
-@Test	
-	public void LoginAudit() throws InterruptedException, IOException
+public class VehicleStaus extends baseLoginTest {
+@Test
+	public void vehiclestatus() throws InterruptedException, IOException
 	{
 	Login loginPage = new Login(driver);
     loginPage.loginTest("Admin","Admin@123$");
     Thread.sleep(3000);
-    
     WebElement Report = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//li[@id='ctl00_Reportmenu']")));
     Report.click();
 	Thread.sleep(3000);
+	
     WebElement scrollup = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("ctl00_liVehicleStatusReport")));
     JavascriptExecutor scroll =(JavascriptExecutor)driver;
     scroll.executeScript("arguments[0].scrollIntoView({block:'center'});",scrollup);
 	
 	Thread.sleep(3000);
-	WebElement LoginAudit = wait.until(ExpectedConditions.elementToBeClickable(By.id("ctl00_liVTSLoginAuditHistoryReport")));
-	LoginAudit.click();
+	WebElement vehiclestatus = wait.until(ExpectedConditions.elementToBeClickable(By.id("ctl00_liVehicleStatusReport")));
+	vehiclestatus.click();
 	
 	Thread.sleep(3000);
 
 	WebElement vendorname = wait.until(ExpectedConditions.elementToBeClickable(By.id("ctl00_ContentPlaceHolder2_ddlVendorName")));
-    /* vendorname.click();
+   /*  vendorname.click();
 	Select dropdown = new Select(vendorname);
 	dropdown.selectByIndex(2);*/
 	
-	
 	Thread.sleep(3000);
 	WebElement Branchname= wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//select[@tabindex='2']")));
-	 /*  Branchname.click();
+	 Branchname.click();
  	Select ddBname =new Select(Branchname);
- 	ddBname.selectByIndex(1);*/
-	
-	
-Thread.sleep(3000);
-	WebElement Username= wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("ctl00_ContentPlaceHolder2_CmbUserName")));
-	 Username.click();
- 	Select ddUsername =new Select(Username);
- 	ddUsername.selectByIndex(1);
-	
-		WebDriverWait wait9 = new WebDriverWait(driver, Duration.ofSeconds(30));
-	WebElement datePickerS = wait9.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id='ctl00_ContentPlaceHolder2_txtStartDate']")));
-	datePickerS.sendKeys("02/12/2025");
-
-Thread.sleep(3000); 
+ 	ddBname.selectByIndex(1);
+ 	 Branchname.click();
+ 	Thread.sleep(3000);
+ 	WebElement vehicleNumber = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("ctl00_ContentPlaceHolder2_CmbVehicleName")));
+ 	vehicleNumber.click();
+ 	/* Select dropdownvNumber =new Select(vehicleNumber);
+ 	 Thread.sleep(1000);
+ 	  int ddnvNumber =dropdownvNumber.getOptions().size();  
+ 	   for( int i=0;i<ddnvNumber ;i++)
+ 	   {
+ 		   Thread.sleep(1000);
+ 		  vehicleNumber = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("ctl00_ContentPlaceHolder2_CmbVehicleName")));
+ 		  dropdownvNumber =new Select(vehicleNumber);
+ 		  dropdownvNumber.selectByIndex(i);
+ 		   
+ 		   
+ 	   }*/
+ 	Thread.sleep(3000); 
 	WebElement timeInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//input[@type='time'])[1]")));
 	timeInput.clear();
 	timeInput.sendKeys("00:11"); 
@@ -79,13 +80,24 @@ Thread.sleep(3000);
 	WebElement reportbtn = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("ctl00_ContentPlaceHolder2_btnReport")));
 	reportbtn.click();
 	Thread.sleep(3000);
-
-			 
-
-			
+ 	
+ 	
+ 	
 	
+       
+
 
 	
-}	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}	
 	
 }
